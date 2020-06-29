334bed1266a30cddac36986727b8bb31d42d0b86
Simon Willison <swillison@gmail.com>
2020-06-29 09:23:11 -0700

    Less strict test

--- test_datasette_debug_asgi.py
+++ test_datasette_debug_asgi.py
@@ -15,23 +15,8 @@ async def test_datasette_debug_asgi():
             assert "text/plain; charset=UTF-8" == response.headers["content-type"]
             assert {
                 "asgi": {"version": "3.0"},
-                "client": ("127.0.0.1", 123),
-                "headers": [
-                    (b"host", b"localhost"),
-                    (
-                        b"user-agent",
-                        b"python-httpx/" + httpx.__version__.encode("utf-8"),
-                    ),
-                    (b"accept", b"*/*"),
-                    (b"accept-encoding", b"gzip, deflate"),
-                    (b"connection", b"keep-alive"),
-                ],
                 "http_version": "1.1",
                 "method": "GET",
                 "path": "/-/asgi-scope",
-                "query_string": b"",
-                "root_path": "",
-                "scheme": "http",
-                "server": "localhost",
                 "type": "http",
-            } == eval(response.content)
+            }.items() <= eval(response.content).items()
