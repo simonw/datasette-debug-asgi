35575f7cd5046d0272548942f737e2fcc40a43ec
Simon Willison <swillison@gmail.com>
2021-01-29 07:25:03 -0800

    Switch to register_routes plugin hook

--- datasette_debug_asgi.py
+++ datasette_debug_asgi.py
@@ -1,30 +1,12 @@
-from pprint import pformat
 from datasette import hookimpl
+from datasette.utils.asgi import Response
+from pprint import pformat
+
+
+def asgi_scope(scope):
+    return Response.text(pformat(scope))
 
 
 @hookimpl
-def asgi_wrapper(datasette):
-    def wrap_with_asgi_auth(app):
-        async def wrapped_app(scope, recieve, send):
-            if scope["type"] == "http" and scope.get("path") == "/-/asgi-scope":
-                await send(
-                    {
-                        "type": "http.response.start",
-                        "status": 200,
-                        "headers": [
-                            [b"content-type", b"text/plain; charset=UTF-8"],
-                        ],
-                    }
-                )
-                await send(
-                    {
-                        "type": "http.response.body",
-                        "body": pformat(scope).encode("utf8"),
-                    }
-                )
-            else:
-                await app(scope, recieve, send)
-
-        return wrapped_app
-
-    return wrap_with_asgi_auth
+def register_routes():
+    return (("^/-/asgi-scope$", asgi_scope),)
--- test_datasette_debug_asgi.py
+++ test_datasette_debug_asgi.py
@@ -7,11 +7,14 @@ async def test_datasette_debug_asgi():
     ds = Datasette([], memory=True)
     response = await ds.client.get("/-/asgi-scope")
     assert 200 == response.status_code
-    assert "text/plain; charset=UTF-8" == response.headers["content-type"]
+    assert "text/plain; charset=utf-8" == response.headers["content-type"]
+    lines = response.text.split("\n")
+    # Remove the csrftoken line, it won't eval() correctly
+    lines = [l for l in lines if "csrftoken" not in l]
     assert {
         "asgi": {"version": "3.0"},
         "http_version": "1.1",
         "method": "GET",
         "path": "/-/asgi-scope",
         "type": "http",
-    }.items() <= eval(response.content).items()
+    }.items() <= eval("\n".join(lines)).items()
